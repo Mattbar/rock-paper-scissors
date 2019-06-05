bc3aa3cd4497833a9b23af4547c8299d85dcf94e
Mattbar <mlbarnes04@gmail.com>
2019-06-05 11:59:11 -0700

    Add changes to get game under tests.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -63,25 +63,48 @@ describe 'A game of rock paper scissors' do
         expect(game.play(:rock, :scissors)).to eq("Rock beats scissors!")
         expect(game.play(:scissors, :rock)).to eq("Rock beats scissors!")
       end
-      skip('is finished')
+      it 'finishes the game'do
+        game = Game.new
+        game.start
+        game.play(:rock, :scissors)
+        expect(game).to be_finished
+      end
     end
 
     context 'rock vs paper' do
-      skip('returns "Paper beats rock!"')
+      it 'knows paper beats rock'do
+        game = Game.new
+        game.start
+        expect(game.play(:rock, :paper)).to eq("Paper beats rock!")
+        expect(game.play(:paper, :rock)).to eq("Paper beats rock!")
+      end
       skip('is finished')
     end
 
 
     context 'scissors vs paper' do
-      skip('returns "Scissors vs paper!"')
+      it 'knows scissors beats paper'do
+        game = Game.new
+        game.start
+        expect(game.play(:scissors, :paper)).to eq("Scissors beats paper!")
+        expect(game.play(:paper, :scissors)).to eq("Scissors beats paper!")
+      end
       skip('is finished')
     end
 
     context 'a tie game' do
-      skip('returns "Tie game. Try again!"')
+      it 'knows a tie' do
+        game = Game.new
+        game.start
+        expect(game.play(:scissors, :scissors)).to eq("Tie game. Try again!")
+        expect(game.play(:paper, :paper)).to eq("Tie game. Try again!")
+        expect(game.play(:rock, :rock)).to eq("Tie game. Try again!")
+      end
       skip('is not finished')
     end
 
   end
 
 end
+
+# Ran out of time, but need to refactor game creation so we can use the same game in the tests. needs to create a fished workflow to get finshed check to pass. refactor game to remove bad if elseif. could use a factory to create multiple game plays. 
